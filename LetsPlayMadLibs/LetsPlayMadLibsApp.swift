8d5a871f366d3b89fa1bef37f97e57cee4d67c08
Farshad Esnaashari <farshad.esnaashari@gmail.com>
2024-04-09 14:33:42 -0500

    Initial Commit

---
+++ LetsPlayMadLibs/LetsPlayMadLibsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LetsPlayMadLibsApp.swift
+//  LetsPlayMadLibs
+//
+//  Created by Farshad Esnaashari on 4/9/24.
+//
+
+import SwiftUI
+
+@main
+struct LetsPlayMadLibsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
